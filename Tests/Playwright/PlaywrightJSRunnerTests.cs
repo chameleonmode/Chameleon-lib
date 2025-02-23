9a8edb71524986c000658a9d25680579d8cb69f6
ezexe@mac <elimdadia@gmail.com>
2025-02-23 08:09:09 -0600

    Refactor PlaywrightJSRunnerTests for consistency; add TestRecord method for script recording

--- Tests/Playwright/PlaywrightJSRunnerTests.cs
+++ Tests/Playwright/PlaywrightJSRunnerTests.cs
@@ -6,19 +6,16 @@ using Chameleon.lib.WebBrowser.Services;
 using static Chameleon.lib.Common.Constants.Enums;
 
 namespace Tests.Playwright;
-public class PlaywrightJSRunnerTests : TestSetup
-{
+public class PlaywrightJSRunnerTests : TestSetup {
 	readonly PlaywrightScriptRepository repo;
 	readonly SystemBrowserService browserService;
 
-	public PlaywrightJSRunnerTests()
-	{
+	public PlaywrightJSRunnerTests() {
 		repo = PlaywrightScriptRepository.Instance;
 		browserService = SystemBrowserService.Instance;
 	}
 
-	async Task<int> OpenBrowser(SystemBrowserType bt = SystemBrowserType.Chrome, int id = 28296)
-	{
+	async Task<int> OpenBrowser(SystemBrowserType bt = SystemBrowserType.Chrome, int id = 28296) {
 		var port = TcpUtil.NextFreePort(9613);
 		var browser = await browserService.OpenWithSettings(new(
 				new(bt, new() { Id = id }),
@@ -32,28 +29,33 @@ public class PlaywrightJSRunnerTests : TestSetup
 		return port;
 	}
 	[Fact]
-	public async Task TestOpenBrowser()
-	{
+	public async Task TestOpenBrowser() {
 		var port = await OpenBrowser();
 		Assert.True(port > 0);
 	}
 
 	[Fact]
-	public async Task TestReddit1CommentScripts()
-	{
+	public async Task TestReddit1CommentScripts() {
 		var port = await OpenBrowser();
 		var cancellationToken = new CancellationToken();
-		await PlaywriteRunner.RunScript(new PlaywriteRunScriptOptions
-		{
+		await PlaywriteRunner.RunScript(new() {
 			Port = port,
 			BundledJSScript = repo.BundledJSScripts[nameof(Reddit1Comment)],
-			Description = new PlaywrightScriptDescription
-			{
+			Description = new PlaywrightScriptDescription {
 				Parameters = [
 					new() { Key = "search", Value = "tangy" },
 					new() { Key = "comment", Value = "rabba luba dub dub" }
 			]
 			}
 		}, cancellationToken);
+	}
+
+	[Fact]
+	public async Task TestRecord() {
+		var port = await OpenBrowser();
+		await PlaywriteRunner.RunScript(new PlaywriteRunScriptOptions {
+			Port = port,
+			Record = true
+		}, CancellationToken.None);
 	}
 }
