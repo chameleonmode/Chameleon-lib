da6302e8bede1db03051f98d6876f075aa331bce
ezexe@mac <elimdadia@gmail.com>
2025-03-06 09:54:29 +0200

    Update AddressUri in Client class for local debugging: switch to localhost URL in DEBUG mode.

--- Chameleon.lib/Abs/Platformatic/Shared/Client.cs
+++ Chameleon.lib/Abs/Platformatic/Shared/Client.cs
@@ -21,8 +21,7 @@ public class Client {
 	//
 	public string AddressUri { get; } =
 		#if DEBUG
-					//"http://127.0.0.1:3042"
-					"https://chameleon-ws-pr-10.onrender.com"
+					"http://127.0.0.1:3042"
 		#else
 					"https://chameleon-ws.onrender.com"
 		#endif
