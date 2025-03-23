fd1679c0fb378e97d36650014ff5c8ad8bc05c2b
ezexe@mac <elimdadia@gmail.com>
2025-03-23 13:27:41 +0200

    Update Reddit comment script test; change search parameter value for accuracy

--- Tests/Playwright/PlaywrightRunnerTests.cs
+++ Tests/Playwright/PlaywrightRunnerTests.cs
@@ -80,14 +80,14 @@ public class PlaywrightRunnerTests : TestSetup {
 
 	[Fact]
 	public async Task TestRedditCommentScript() {
-		var port = 9613; 
-		//var port = await OpenBrowser();
+		//var port = 9613; 
+		var port = await OpenBrowser();
 		await PlaywriteRunner.RunScript(new() {
 			Port = port,
 			BundledScript = repo.BundledJSScripts[nameof(Comment)],
 			Description = new(
 				Parameters: new() {
-					{"search", "mumbo jumbo"}
+					{"search", "plumbus"}
 				}
 			)
 		});
