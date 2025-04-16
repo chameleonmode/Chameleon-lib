c623021cebd218c8768c4ae75cf5067da936157e
ezexe@mac <elimdadia@gmail.com>
2025-04-16 17:38:11 +0300

    Refactor Session class: move LoginSettings, TokenResponse, and TokenPayload records into Session.cs; improve method formatting for consistency

--- Chameleon.lib/Auth/Session.cs
+++ Chameleon.lib/Auth/Session.cs
@@ -2,29 +2,39 @@
 using System.Net.Http.Headers;
 
 namespace Chameleon.lib.Auth;
-public class Session {
+
+public class Session
+{
 	Session() { }
 
 	public Client Auth0Client { get; } = new();
 	public LoginSettings? Login { get; private set; }
 
-	public void SetLogin(LoginSettings login) {
+	public void SetLogin(LoginSettings login)
+	{
 		Login = login;
 		IoC.SetJsonValue(login, nameof(LoginSettings));
 	}
 
-	public async Task<(Client, AuthenticationHeaderValue)> Authenticate() {
+	public async Task<(Client, AuthenticationHeaderValue)> Authenticate()
+	{
 		return (Auth0Client, await Auth0Client.TryLogIn());
 	}
 
-	public async Task Logout() {
-		try {
+	public async Task Logout()
+	{
+		try
+		{
 			await Auth0Client.Logout();
 			Auth0Client.Authorization = null;
 			IoC.ClearValue(nameof(TokenResponse));
-		} catch (Exception){
+		}
+		catch (Exception)
+		{
 			// ignore for now
-		} finally {
+		}
+		finally
+		{
 			if (Login != null)
 				IoC.SetJsonValue(new LoginSettings(Login.LoginName, Login.LicenseKey, false), nameof(LoginSettings));
 		}
@@ -33,3 +43,28 @@ public class Session {
 	// singleton	
 	public static Session Instance { get; } = new();
 }
+
+public record LoginSettings(string LoginName, string LicenseKey, bool AutoLogin = true);
+
+#pragma warning disable IDE1006 // Naming Styles
+public record TokenResponse(
+		string access_token,
+		string refresh_token,
+		string id_token,
+		string Scope,
+		int expires_in,
+		string token_type
+);
+
+public record TokenPayload(
+		string iss,
+		string sub,
+		string[] aud,
+		int iat,
+		int exp,
+		string scope,
+		string azp,
+		object[] permissions
+);
+
+#pragma warning restore IDE1006 // Naming Styles
