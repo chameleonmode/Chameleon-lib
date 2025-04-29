e430a041d61effdf466116d1c756ac969b377ae8
ezexe@mac <elimdadia@gmail.com>
2025-04-29 10:31:31 +0300

    refactor: Update AddProfiles and Share methods to use IEnumerable<int> and improve null handling refactor: Remove unused extension methods from BoolExts, EnumerableExts, and ObjExts chore: Add Extensions folder to project structure chore: Update Chameleon.lib.csproj to include System.Management package

--- Chameleon.lib/Util/Extensions.cs
+++ Chameleon.lib/Util/Extensions.cs
@@ -1,8 +1,11 @@
 namespace Chameleon.lib.Util;
-public static class Extensions {
-	public static TResult Let<T, TResult>(this T self, Func<T, TResult> function) => function(self);
 
-  public static bool IsSimpleType(this Type type) {
+public static class Extensions
+{
+  public static TResult Let<T, TResult>(this T self, Func<T, TResult> function) => function(self);
+
+  public static bool IsSimpleType(this Type type)
+  {
     return type.IsPrimitive ||
            type == typeof(string) ||
            type == typeof(decimal) ||
@@ -11,5 +14,26 @@ public static class Extensions {
            type == typeof(DateTimeOffset) ||
            type == typeof(TimeSpan) ||
            type == typeof(Guid);
+  }
+
+
+  public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+  {
+    foreach (var item in source)
+      action(item);
+  }
+  public static async Task ForEach<T>(this IEnumerable<T> source, Func<T, Task> action)
+  {
+    foreach (var item in source)
+      await action(item);
+  }
+
+  public static async Task Empty<T>(this IList<T> source, Func<T, Task<bool>> predicate)
+  {
+    for (var i = source.Count - 1; i >= 0; i--)
+    {
+      if (await predicate(source.ElementAt(i)))
+        source.RemoveAt(i);
+    }
   }
 }
