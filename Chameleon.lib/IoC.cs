60e59110eeef3c3aa05914356010ada94f8f5a6b
ezexe@mac <elimdadia@gmail.com>
2025-07-07 07:06:12 +0300

    refactor: Update JSON handling in J2on class and improve null safety in Stringify method

--- Chameleon.lib/IoC.cs
+++ Chameleon.lib/IoC.cs
@@ -50,8 +50,9 @@ public static class JSON {
 
 	public static string Serialize(object o, JsonSerializerOptions? options = null) =>
 	 JsonSerializer.Serialize(o, options ?? InsensitiveCamelCaseOptions);
-	public static string? Stringify(object? o, JsonSerializerOptions? options = null) =>
-	 o != null ? Serialize(o, options) : null;
+	public static string Stringify(object? o, JsonSerializerOptions? options = null) {
+		return Serialize(o ?? throw new ArgumentNullException(nameof(o)), options);
+	}
 
 	public class DynamicJsonConverter<T1, T2> : JsonConverter<T2> where T1 : T2 {
 		public override T2? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
--- Tests/Abs/PFtests.cs
+++ Tests/Abs/PFtests.cs
@@ -3,53 +3,31 @@ using Chameleon.lib;
 using Chameleon.lib.Abs;
 namespace Tests.Abs;
 
-public class J2on<T> {
-	internal string? Jzon { get; set; }
-
-	internal List<T> O {
-    get => JSON.Parse<List<T>>(Jzon);
-    set => Jzon = JSON.Stringify(value);
-  }
-}
-public record Proxzy(string? Host = null, int Port = 0, string? UserName = null, string? Password = null);
-# region dtos
 public interface IID {
-  int Id { get; init; }
-}
-public interface IT {
-  string TenantId { get; init; }
-}
-public interface IDT : IT {
   int? Id { get; init; }
-  string? Jzon { get; set; }
 }
-public record ID : IID {
-  public int Id { get; init; }
-}
-public record I : IT {
-  public required string TenantId { get; init; }
-}
-public record Tenant(string TenantId) : ID;
-public record Permission(string Name, string Description) : ID;
-public record UserPermission(int PermissionId, int UserId) : I;
-public record UserAccess(int UserId, int ProfileId, int FolderId) : I;
-public record DT<T> : I, IDT {
-  public int? Id { get; init; }
-  public string? Jzon { get; set; }
-  public T? O {
+public record J2on<T> {
+  public string Jzon { get; set; } = string.Empty;
+  public T O {
     get => JSON.Parse<T>(Jzon);
     set => Jzon = JSON.Stringify(value);
   }
 }
 
-public record EE  {
+public record EE {
   public required string Title { get; init; }
   public bool IsFavourite { get; set; } = false;
   public string? Notes { get; set; }
 }
 public record Folderee : EE;
-public record Folder : DT<Folderee>;
-
+public record Profilee : EE {
+  public Proxzy Proxy { get; set; } = new();
+  public record Proxzy(string? Host, int Port, string? UserName, string? Password) {
+    public Proxzy() : this(null, 0, null, null) { }
+    public Proxzy(string? host) : this(host, 0, null, null) { }
+    public Proxzy(string? host, int port) : this(host, port, null, null) { }
+  };
+}
 public record Addressesee : EE {
   public int? CountryId { get; set; }
   public string? AddressLine1 { get; set; }
@@ -58,21 +36,18 @@ public record Addressesee : EE {
   public string? State { get; set; }
   public string? Zip { get; set; }
 }
-
 public record Businessesee : EE {
   public string? CompanyName { get; set; }
   public string? Department { get; set; }
   public string? PhoneNumber { get; set; }
   public string? WebSite { get; set; }
 }
-
 public record Loginsee : EE {
   public string? WebSite { get; set; }
   public string? Email { get; set; }
   public string? UserName { get; set; }
   public string? Password { get; set; }
 }
-
 public record Personesee : EE {
   public string? FirstName { get; set; }
   public string? LastName { get; set; }
@@ -84,63 +59,70 @@ public record Personesee : EE {
   public DateTimeOffset BirthDateOffset => new(BirthDate);
 }
 
-public record Profilee : EE {
-  public Proxzy Proxy { get; set; } = new();
+# region dtos
+public record ID : IID {
+  public int? Id { get; init; }
 }
+public record Tenant(string TenantId) : ID;
+public record Permission(string Name, string Description) : ID;
 
+public record DT<T> : J2on<T>, IID {
+  public int? Id { get; init; }
+  public required string TenantId { get; init; }
+}
+public record Folder : DT<Folderee>;
 public record Profile : DT<Profilee> {
   public int? FolderId { get; init; } = null;
-
-  public J2on<Addressesee> Addresses { get; set; } = new();
-  public string? Addressez { get => Addresses.Jzon; set => Addresses.Jzon = value; }
-
-  public J2on<Businessesee> Businesses { get; set; } = new();
-  public string? Businessez { get => Businesses.Jzon; set => Businesses.Jzon = value; }
-
-  public J2on<Loginsee> Logins { get; set; } = new();
-  public string? Loginz { get => Logins.Jzon; set => Logins.Jzon = value; }
-
-  public J2on<Personesee> Persons { get; set; } = new();
-  public string? Personz { get => Persons.Jzon; set => Persons.Jzon = value; }
 }
+
+public record Identity<T> : DT<List<T>> {
+  public required int ProfileId { get; init; }
+}
+public record Addressez : Identity<Addressesee>;
+public record Businessez : Identity<Businessesee>;
+public record Personz : Identity<Personesee>;
+public record Loginz : Identity<Loginsee>;
 #endregion
 
 public class Table<T, TT>(string prefix) : Web where T : DT<TT> {
   public Request Req { get; } = new(prefix + '/', Authenticate: !Debugger.IsAttached);
-  public DT<TT> DTO => new() { TenantId = PF.I.Tenant.TenantId };
+
+  public virtual DT<TT> DTO => new() { TenantId = PF.I.Tenant.TenantId };
+
   public async Task<IEnumerable<T>?> Get() => await Get<IEnumerable<T>>(Req);
   public async Task<IEnumerable<T>?> Get(string q) => await Get<IEnumerable<T>>(Req with { Q = q });
-  public async Task<T?> Get(int id) => await Get<T>(Req with { Path = $"{Req.Path}{id}" });
-  public Task<T?> Create(TT entitee) => Post<T>(Req with {
-    Body =  DTO with {
-      O = entitee
-    }
-  });
-  public async Task<T?> Update(T dt) =>await Put<T>(Req with {
-    Path = $"{Req.Path}{dt.Id}",
-    Body = dt
-  });
-  
-  public async Task<T?> Delete(int id) => await Delete<T>(Req with { Path = $"{Req.Path}{id}" });
+  public async Task<T?> Get(int? id) => await Get<T>(Req with { Path = $"{Req.Path}{id}" });
+
+  public Task<T?> Create(T dt) => Post<T>(Req with { Body = dt });
+  public Task<T?> Create(TT entitee) => Post<T>(Req with { Body = DTO with { O = entitee } });
+
+  public async Task<T?> Update(T dt) => await Put<T>(Req with { Path = $"{Req.Path}{dt.Id}", Body = dt });
+
+  public async Task<T?> Delete(int? id) => await Delete<T>(Req with { Path = $"{Req.Path}{id}" });
 }
 public class PF {
   public Tenant Tenant { get; set; } = new("b6633ec1-138f-4ec6-b9d0-71b0660c0a44");
-  public Table<DT<Folderee>, Folderee> Folders { get; } = new("folders");
+  public Table<Folder, Folderee> Folders { get; } = new("folders");
   public Table<Profile, Profilee> Profiles { get; } = new("profiles");
+  public Table<Addressez, List<Addressesee>> Addressez { get; } = new("addressez");
+  public Table<Businessez, List<Businessesee>> Businessez { get; } = new("businessez");
+  public Table<Personz, List<Personesee>> Personz { get; } = new("personz");
+  public Table<Loginz, List<Loginsee>> Loginz { get; } = new("loginsz");
 
   PF() { }
   public static PF I { get; } = new();
 }
 
 public class Tests : TestSetup {
+  #region  Folders
   [Fact]
   public async Task CreateFolder_ShouldReturnValidFolder() {
-    var result = await PF.I.Folders.Create(new() { Title = "Folder" });
+    var result = await PF.I.Folders.Create(new Folderee() { Title = "Folder" });
 
     Assert.NotNull(result);
     Assert.Equal(PF.I.Tenant.TenantId, result.TenantId);
     Assert.NotNull(result.O);
-    Assert.Equal("Folder", result.O?.Title);
+    Assert.Equal("Folder", result.O.Title);
   }
 
   [Fact]
@@ -192,6 +174,8 @@ public class Tests : TestSetup {
     Assert.Null(folder.O);
   }
 
+  #endregion
+
   // .........
   // Profile tests
   // .........
@@ -241,57 +225,57 @@ public class Tests : TestSetup {
   }
 
   [Fact]
-  public async Task GetProfiles_ShouldReturnCollection() {
-    var get = await PF.I.Profiles.Get(1);
+  public async Task Identitiez_Create() {
+    var created = await PF.I.Addressez.Create(new Addressez {
+      TenantId = PF.I.Tenant.TenantId,
+      ProfileId = 1,
+      O = [new Addressesee { Title = "Test Address", AddressLine1 = "123 Main St", City = "Test City", State = "TS", Zip = "12345" }]
+    });
+    Assert.NotNull(created);
+
+    var get = await PF.I.Addressez.Get(created.Id);
     Assert.NotNull(get);
 
-    var result = await PF.I.Profiles.Update(get with {
-      Addresses = new() {
-        O = [
-        new() { Title = "Test Address 1", AddressLine1 = "123 Main St", City = "Test City", State = "TS", Zip = "12345" },
-        new() { Title = "Test Address 2", AddressLine1 = "456 Elm St", City = "Another City", State = "AS", Zip = "67890" }
-      ]
-      },
-      Businesses = new() {
-        O = [
-          new() { Title = "Test Business 1", CompanyName = "Test Company", PhoneNumber = "123-456-7890", WebSite = "https://testcompany.com" },
-          new() { Title = "Test Business 2", CompanyName = "Another Company", PhoneNumber = "987-654-3210", WebSite = "https://anothercompany.com" }
-        ]
-      },
-      Persons = new() {
-        O = [
-          new() { Title = "Test Person 1", FirstName = "John", LastName = "Doe", Email = "john.doe@example.com" },
-          new() { Title = "Test Person 2", FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com" }
-        ]
-      },
-      Logins = new() {
-        O = [
-          .. get.Logins.O ?? [], // Keep existing logins
-          new() { Title = "Test Login 1", WebSite = "https://testlogin1.com", UserName = "user1", Password = "pass1" },
-          new() { Title = "Test Login 2", WebSite = "https://testlogin2.com", UserName = "user2", Password = "pass2" }
-        ]
-      }
-    });
 
-    Assert.NotNull(result);
-    Assert.NotNull(result.Addressez);
-    Assert.NotNull(result.Businessez);
-    Assert.NotNull(result.Personz);
-    Assert.NotNull(result.Loginz);
+    // var result = await PF.I.Addressez.Update(get with {
+    //   Addresses = new() {
+    //     O = [
+    //     new() { Title = "Test Address 1", AddressLine1 = "123 Main St", City = "Test City", State = "TS", Zip = "12345" },
+    //     new() { Title = "Test Address 2", AddressLine1 = "456 Elm St", City = "Another City", State = "AS", Zip = "67890" }
+    //   ]
+    //   },
+    //   Businesses = new() {
+    //     O = [
+    //       new() { Title = "Test Business 1", CompanyName = "Test Company", PhoneNumber = "123-456-7890", WebSite = "https://testcompany.com" },
+    //       new() { Title = "Test Business 2", CompanyName = "Another Company", PhoneNumber = "987-654-3210", WebSite = "https://anothercompany.com" }
+    //     ]
+    //   },
+    //   Persons = new() {
+    //     O = [
+    //       new() { Title = "Test Person 1", FirstName = "John", LastName = "Doe", Email = "john.doe@example.com" },
+    //       new() { Title = "Test Person 2", FirstName = "Jane", LastName = "Smith", Email = "jane.smith@example.com" }
+    //     ]
+    //   },
+    //   Logins = new() {
+    //     O = [
+    //       .. get.Logins.O ?? [], // Keep existing logins
+    //       new() { Title = "Test Login 1", WebSite = "https://testlogin1.com", UserName = "user1", Password = "pass1" },
+    //       new() { Title = "Test Login 2", WebSite = "https://testlogin2.com", UserName = "user2", Password = "pass2" }
+    //     ]
+    //   }
+    // });
+
+    // Assert.NotNull(result);
+    // Assert.NotNull(result.Addressez);
+    // Assert.NotNull(result.Businessez);
+    // Assert.NotNull(result.Personz);
+    // Assert.NotNull(result.Loginz);
   }
   [Fact]
   public async Task GetProfilesAddresses_ShouldReturnEmptyAddressCollection() {
-    var get = await PF.I.Profiles.Get(1);
-    Assert.NotNull(get);
-    var result = await PF.I.Profiles.Update(
-      get with {
-        Addressez = null
-      }
-    );
+    var result = await PF.I.Addressez.Get();
 
     Assert.NotNull(result);
-    Assert.NotNull(result.Businessez);
-    Assert.Empty(result.Addresses.O); // TODO
   }
 
   // [Fact]
